7e047acd4eef62d62bd0863400009d6b40959d9f
adlanikawtar <adlanikawtar@gmail.com>
2024-04-28 17:58:19 +0100

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(reservation::class);
     }
+    public function cart()
+    {
+        return $this->hasOne(CarteModel::class);
+    }
 }
